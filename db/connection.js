6ba4c7ff71d31c9028cac72bb33bfb1ceba30125
myztajay <myztajay@hotmail.com>
2017-01-24 09:13:38 -0500

    staging before pull of the master

--- db/connection.js
+++ db/connection.js
@@ -5,13 +5,12 @@ mongoose.Promise = global.Promise;
 
 var PoemSchema = new mongoose.Schema(
   {
-      title: String,
-      body: String,
+    title: String,
+    body: String,
 });
 
 mongoose.model('Poem', PoemSchema);
 mongoose.connect("mongodb://localhost/poemDb");
-
 module.exports = mongoose;
 // var seedData = require("./seed.json")
 // module.exports = {
