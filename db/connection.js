64050c7718bf3ef0c5e12f48a0cb8129a46b38c5
myztajay <myztajay@hotmail.com>
2017-01-25 10:15:21 -0500

    Added UserSchema to connection.js

--- db/connection.js
+++ db/connection.js
@@ -9,6 +9,13 @@ var PoemSchema = new mongoose.Schema(
     body: String,
 });
 
+var UserSchema = new mongoose.Schema(
+  {
+    username:String,
+    password:String
+  });
+//test if branch will change
 mongoose.model('Poem', PoemSchema);
+mongoose.model('User', UserSchema);
 mongoose.connect("mongodb://localhost/poemDb");
 module.exports = mongoose;
